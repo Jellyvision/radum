1c748f9d4be0b648cb9378c69e14e459663ccc09
Shaun Rowland <rowland@shaunrowland.com>
2011-01-07 02:48:46 -0500

    Fixes to work with Ruby 1.9 and the new net-ldap gem.

--- lib/radum.rb
+++ lib/radum.rb
@@ -1,4 +1,17 @@
-gem 'ruby-net-ldap', '= 0.0.4'
+# Ruby 1.9.x requirements.
+if RUBY_VERSION =~ /^1.9/
+  require 'yaml'
+  
+  # The net-ldap gem uses String#to_a. In Ruby 1.9.x this needs to be
+  # String.lines.to_a, but I have to monkey patch this in for things to work.
+  class String
+    def to_a
+      [self]
+    end
+  end
+end
+
+gem 'net-ldap', '= 0.1.1'
 require 'net/ldap'
 
 require 'radum/logger'
