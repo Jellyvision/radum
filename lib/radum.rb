59d973fbbf37a9ab810aa2ac6e8b34a0939e8adf
Shaun Rowland <rowland@shaunrowland.com>
2011-01-08 18:58:34 -0500

    Changed gemspec to require Ruby 1.8.7 or higher and use net-ldap.
    The Ruby version is a requirement of net-ldap 0.1.1. I used an exact
    version in case behavior changes in future releases. For some reason I
    was worried about this before.

--- lib/radum.rb
+++ lib/radum.rb
@@ -1,7 +1,5 @@
 # Ruby 1.9.x requirements.
 if RUBY_VERSION =~ /^1.9/
-  require 'yaml'
-  
   # The net-ldap gem uses String#to_a. In Ruby 1.9.x this needs to be
   # String.lines.to_a, but I have to monkey patch this in for things to work.
   class String
@@ -11,6 +9,7 @@ if RUBY_VERSION =~ /^1.9/
   end
 end
 
+require 'yaml'
 gem 'net-ldap', '= 0.1.1'
 require 'net/ldap'
 
